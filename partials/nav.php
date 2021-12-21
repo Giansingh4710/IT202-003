49330f70bd03e7ce445d04c98c9eb39aa4f8f5b7
Gian <giansingh4710@gmail.com>
2021-12-21 02:47:10 -0500

    admins can change role

--- partials/nav.php
+++ partials/nav.php
@@ -57,6 +57,7 @@ require_once(__DIR__ . "/../lib/functions.php");
                             <li><a class="dropdown-item" href="<?php echo get_url('admin/create_role.php'); ?>">Create</a></li>
                             <li><a class="dropdown-item" href="<?php echo get_url('admin/list_roles.php'); ?>">List</a></li>
                             <li><a class="dropdown-item" href="<?php echo get_url('admin/assign_roles.php'); ?>">Assign</a></li>
+                            <li><a class="dropdown-item" href="<?php echo get_url('admin/change_comps.php'); ?>">Change Competitions</a></li>
                         </ul>
                     </li>
                 <?php endif; ?>
